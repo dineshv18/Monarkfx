59581c689715ca654082c93d6195010be67b9ea4
riteshk-007 <codeshorts007@gmail.com>
2026-07-16 17:05:13 +0530

    feat: initialize root layout with global providers, tracking scripts, and SEO metadata

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -38,10 +38,25 @@ export default function RootLayout({
             gtag('config', 'AW-17830556034');
           `}
         </Script>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-53LW22LW');`}
+        </Script>
       </head>
       <body
         className={`${inter.variable} ${spaceGrotesk.variable} antialiased bg-[var(--color-bg-primary)] text-[var(--color-text-mid)] font-body overflow-x-hidden`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-53LW22LW"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {/* Global Tracking Scripts */}
         <TrackingScripts />
 
