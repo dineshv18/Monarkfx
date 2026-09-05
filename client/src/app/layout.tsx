903ffe1f577434365ba03b3c49251ce6bf02d7d3
riteshk-007 <codeshorts007@gmail.com>
2026-09-05 20:31:07 +0530

    feat: initialize root layout with SEO metadata, analytics scripts, and global providers

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -59,7 +59,7 @@ export default function RootLayout({
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-53LW22LW');`}
+})(window,document,'script','dataLayer','GTM-NTZFNHH4');`}
         </Script>
       </head>
       <body
@@ -67,7 +67,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       >
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-53LW22LW"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NTZFNHH4"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
